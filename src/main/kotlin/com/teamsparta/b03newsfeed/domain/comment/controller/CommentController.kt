d1cb6979cf8f0dd6f6d5caf3eeaaa9c853dc2b2b
chanmo2311211 <abczza@naver.com>
2024-01-08 19:29:57 +0900

    댓글컨트롤러

--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
@@ -1,4 +1,36 @@
 package com.teamsparta.b03newsfeed.domain.comment.controller
 
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RequestMapping("/posts/{postId}/comments")
+@RestController
 class CommentController {
+
+    @GetMapping("/{commentId}")
+    fun getComment(){
+        TODO()
+    }
+    @GetMapping
+    fun getCommentList(){
+        TODO()
+    }
+    @PutMapping("/{commentId}")
+    fun updateComment(){
+        TODO()
+    }
+    @PostMapping
+    fun createComment(){
+        TODO()
+    }
+    @DeleteMapping("/{commentId}")
+    fun deleteComment(){
+        TODO()
+    }
+
 }
