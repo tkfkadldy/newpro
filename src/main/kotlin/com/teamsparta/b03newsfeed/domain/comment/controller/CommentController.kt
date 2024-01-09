bd92db69a9046e1f57803329a21ccbb01faf4b42
chanmo2311211 <abczza@naver.com>
2024-01-09 13:43:39 +0900

    CommentService와 CommentController 연결

--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
@@ -1,39 +1,64 @@
 package com.teamsparta.b03newsfeed.domain.comment.controller
 
+import com.teamsparta.b03newsfeed.domain.comment.dto.CommentResponse
+import com.teamsparta.b03newsfeed.domain.comment.dto.CreateCommentRequest
+import com.teamsparta.b03newsfeed.domain.comment.dto.UpdateCommentRequest
+import com.teamsparta.b03newsfeed.domain.comment.service.CommentService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RequestMapping("/posts/{postId}/comments")
 @RestController
-class CommentController {
+class CommentController(
+    private val commentService: CommentService
+) {
 
     @GetMapping("/{commentId}")
-    fun getComment(){
-        TODO()
+    fun getComment(@PathVariable commentId:Long): ResponseEntity<CommentResponse>{
+       return ResponseEntity
+           .status(HttpStatus.OK)
+           .body(commentService.getCommentById(commentId))
     }
 
     @GetMapping
-    fun getCommentList(){
-        TODO()
+    fun getCommentList():ResponseEntity<List<CommentResponse>>{
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(commentService.getAllCommentList())
     }
     @PutMapping("/{commentId}")
-    fun updateComment(){
-        TODO()
+    fun updateComment(
+        @PathVariable commentId: Long,
+        @RequestBody updateCommentRequest: UpdateCommentRequest
+    ):ResponseEntity<CommentResponse>
+    {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(commentService.updateComment(commentId,updateCommentRequest))
     }
 
     @PostMapping
-    fun createComment(){
-        TODO()
+    fun createComment(@RequestBody createCommentRequest: CreateCommentRequest):ResponseEntity<CommentResponse>{
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(commentService.createComment(createCommentRequest))
     }
 
     @DeleteMapping("/{commentId}")
-    fun deleteComment(){
-        TODO()
+    fun deleteComment(@PathVariable commentId: Long):ResponseEntity<Unit>{
+        commentService.deleteComment(commentId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
 
 }
