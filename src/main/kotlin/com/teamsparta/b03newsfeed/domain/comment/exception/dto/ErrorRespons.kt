202c6f3be2be1ea280b7f101622c5a9032170bd6
chanmo2311211 <abczza@naver.com>
2024-01-09 14:48:14 +0900

    comment 예외처리

---
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/exception/dto/ErrorRespons.kt
@@ -0,0 +1,5 @@
+package com.teamsparta.b03newsfeed.domain.comment.exception.dto
+
+data class ErrorResponse(
+    val message: String?
+)
--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/service/CommentServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/service/CommentServiceImpl.kt
@@ -3,26 +3,31 @@ package com.teamsparta.b03newsfeed.domain.comment.service
 import com.teamsparta.b03newsfeed.domain.comment.dto.CommentResponse
 import com.teamsparta.b03newsfeed.domain.comment.dto.CreateCommentRequest
 import com.teamsparta.b03newsfeed.domain.comment.dto.UpdateCommentRequest
+import com.teamsparta.b03newsfeed.domain.comment.exception.CommentNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class CommentServiceImpl :CommentService{
+class CommentServiceImpl : CommentService {
     override fun getAllCommentList(): List<CommentResponse> {
         TODO("Not yet implemented")
     }
 
     override fun getCommentById(commentId: Long): CommentResponse {
-        TODO("Not yet implemented")
+        throw CommentNotFoundException(commentName = "Comment", commentId = 1L )
     }
 
+    @Transactional
     override fun createComment(request: CreateCommentRequest): CommentResponse {
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun updateComment(commentId: Long, request: UpdateCommentRequest): CommentResponse {
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun deleteComment(commentId: Long) {
         TODO("Not yet implemented")
     }
