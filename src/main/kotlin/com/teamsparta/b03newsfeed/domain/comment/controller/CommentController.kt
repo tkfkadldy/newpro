d9ea6b7bda9bf17d0ab06d6dc4a7d9ebc397515c
chanmo2311211 <abczza@naver.com>
2024-01-10 09:41:59 +0900

    테스트

--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/comment/controller/CommentController.kt
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-
+//테스트
 
 @RequestMapping("/posts/{postId}/comments")
 @RestController
