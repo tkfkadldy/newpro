44bad18dbbb05dafff004e073a326dd2fe6f8107
tkfadldy <kimmin20012@gmail.com>
2024-01-10 22:56:25 +0900

    post

--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/post/exception/PostNotFoundException.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/post/exception/PostNotFoundException.kt
@@ -1,4 +1,4 @@
 package com.teamsparta.b03newsfeed.domain.post.exception
 
-data class PostNotFoundException(val postName: String, val postnumid: Long?) :
-    RuntimeException("$postName not found with given id: $postnumid")
+data class PostNotFoundException(val postName: String, val postid: Long?) :
+    RuntimeException("$postName not found with given id: $postid")
--- src/main/kotlin/com/teamsparta/b03newsfeed/domain/post/service/PostServicelmpl.kt
+++ src/main/kotlin/com/teamsparta/b03newsfeed/domain/post/service/PostServicelmpl.kt
@@ -1,41 +1,57 @@
 package com.teamsparta.b03newsfeed.domain.post.service
 
+import com.teamsparta.b03newsfeed.domain.comment.repository.CommentRepository
 import com.teamsparta.b03newsfeed.domain.post.dto.CreatePostRequest
 import com.teamsparta.b03newsfeed.domain.post.dto.PostResponse
 import com.teamsparta.b03newsfeed.domain.post.dto.UpdatePostRequest
+import com.teamsparta.b03newsfeed.domain.post.exception.PostNotFoundException
+import com.teamsparta.b03newsfeed.domain.post.model.Post
+import com.teamsparta.b03newsfeed.domain.post.repository.PostRepository
+import com.teamsparta.b03newsfeed.domain.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PostServicelmpl:PostService {
+class PostServicelmpl(
+    private val postRepository: PostRepository,
+    private val commentRepository: CommentRepository,
+    private val userRepository: UserRepository,
+) {
     override fun getAllPostList(): List<PostResponse> {
-        TODO("Not yet implemented")
-        //TODO: DB에서 모든 post를 가져와서 PostResponse로 변환 후 반환
+        return postRepository.findAll().map { it.toResponse() }
     }
-
     override fun getPostById(postId: Long): PostResponse {
-        TODO("Not yet implemented")
-        // TODO: 만약 postId에 해당하는 post가 없다면 throw PostNotFoundException
-        // TODO: DB에서 postId에 해당하는 Post를 가져와서 PostResponse로 변환 후 반환
+        val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException("Post", postId)
+        return post.toResponse()
+
     }
 
     @Transactional
     override fun createPost(request: CreatePostRequest): PostResponse {
-        TODO("Not yet implemented")
-        // TODO: request를 post로 변환 후 DB에 저장
+        return postRepository.save(
+            Post(
+                title = request.title,
+                content = request.content,
+            )
+        ).toResponse()
     }
     @Transactional
     override fun updatePost(postId: Long, request: UpdatePostRequest): PostResponse {
-        TODO("Not yet implemented")
-        // TODO: 만약 postId에 해당하는 post가 없다면 throw postNotFoundException
-        // TODO: DB에서 postId에 해당하는 post를 가져와서 request로 업데이트 후 DB에 저장, 결과를 PostResponse로 변환 후 반환
+        val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException("Post", postId)
+        val (title, content) = request
+
+        post.title = title
+        post.content = content
+        post.image = image
+        post.tag = tag
+
+        return postRepository.save(post).toResponse()
     }
 
     @Transactional
     override fun deletePost(postId: Long) {
-        TODO("Not yet implemented")
-        // TODO: 만약 postId에 해당하는 Post가 없다면 throw postNotFoundException
-        // TODO: DB에서 postId에 해당하는 Post를 삭제, 연관된 comment 모두 삭제
+        val course = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException("Post", postId)
+        postRepository.delete(course)
     }
 
 
